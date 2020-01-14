8ad7d494738d8ae8012f320732a09356fc1b3672
DarkElectro <electrodark74@gmail.com>
2020-01-14 17:51:44 +0500

    fix

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,23 @@
+const toolTips = document.querySelectorAll('.has-tooltip');
+const tip = document.createElement('div');
+tip.className = 'tooltip';
+tip.style = 'position: absolute';
+
+for (toolTip of toolTips) {
+  const wrapper = document.createElement('div');
+  wrapper.style = 'display: inline-block';
+  wrapper.className = 'wrapper';
+  wrapper.innerHTML = toolTip.outerHTML;
+  toolTip.replaceWith(wrapper);
+}
+
+const tipLinks = document.querySelectorAll('.wrapper');
+
+for (tipLink of tipLinks) {
+  tipLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    tip.innerText = event.target.title;
+    event.target.parentElement.appendChild(tip);
+    tip.classList.add('tooltip_active');
+  });
+}
