618996475c104a8e078f86fe1d6b3dbdba64ce44
DarkElectro <electrodark74@gmail.com>
2020-01-15 18:10:23 +0500

    fix

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,23 +1,14 @@
-const toolTips = document.querySelectorAll('.has-tooltip');
-const tip = document.createElement('div');
-tip.className = 'tooltip';
-tip.style = 'position: absolute';
+const hasTooltip = document.querySelectorAll(`.has-tooltip`);
+const hasTooltipArr = Array.from(hasTooltip);
+const tooltipDiv = document.createElement(`div`);
 
-for (toolTip of toolTips) {
-  const wrapper = document.createElement('div');
-  wrapper.style = 'display: inline-block';
-  wrapper.className = 'wrapper';
-  wrapper.innerHTML = toolTip.outerHTML;
-  toolTip.replaceWith(wrapper);
-}
-
-const tipLinks = document.querySelectorAll('.wrapper');
-
-for (tipLink of tipLinks) {
-  tipLink.addEventListener('click', function(event) {
+hasTooltip.forEach((el) => {el.addEventListener(`click`, function () {
+    tooltipDiv.classList.add(`tooltip`);
+    tooltipDiv.setAttribute(`style`, `left: ${el.getBoundingClientRect().left}px; top: ${el.getBoundingClientRect().top + 16}px`);
+    tooltipDiv.classList.toggle(`tooltip_active`);
+    tooltipDiv.innerText = `${el.title}`;
+    el.insertAdjacentElement("afterend", tooltipDiv);
+    
     event.preventDefault();
-    tip.innerText = event.target.title;
-    event.target.parentElement.appendChild(tip);
-    tip.classList.add('tooltip_active');
-  });
-}
+    
+})})
